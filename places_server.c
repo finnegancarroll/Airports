3cdc7ab394089a4d0c9f19ee90376d86a4ce1518
finnegancarroll <carrollf@cs1.seattleu.edu>
2019-11-09 17:54:49 -0800

    Example output, only 2 functions left

--- places_server.c
+++ places_server.c
@@ -2,7 +2,8 @@
 #include <errno.h>
 #include <vector>
 
-void freePlaneList(planeListRet list);
+//Takes coordinates of client and outputs 5 closes airport info
+void fiveClosest(position *p, planeListRet &list);
 
 planeListRet *
 query_airports_1_svc(position *argp, struct svc_req *rqstp)
@@ -10,16 +11,41 @@ query_airports_1_svc(position *argp, struct svc_req *rqstp)
   //No need to xdr_free(), no structures allocated on the heap 
   errno = 0;//clear garbage data
 	static planeListRet result_1;
-  
-  //Sever code///////////
- 
-  result_1.planeListRet_u.airp.p = "code: BRW, name: Barrow, state: AK, distance: 2698.693567";
-  result_1.planeListRet_u.airp.port1 = "code: BRW, name: Barrow, state: AK, distance: 2698.693567";
-  result_1.planeListRet_u.airp.port2 = "code: BRW, name: Barrow, state: AK, distance: 2698.693567";
-  result_1.planeListRet_u.airp.port3 = "code: BRW, name: Barrow, state: AK, distance: 2698.693567";
-  result_1.planeListRet_u.airp.port4 = "code: BRW, name: Barrow, state: AK, distance: 2698.693567";
-  result_1.planeListRet_u.airp.port5 = "code: BRW, name: Barrow, state: AK, distance: 2698.693567";
+
+  fiveClosest(argp, result_1);
 	
   result_1.err = errno;
   return &result_1;
 }
+
+void fiveClosest(position *p, planeListRet &list){
+  ////////////IMPLEMENT THIS FUNCTION//////////// 
+  ////////////USE THE COORDINATES(LAT/LON) TO FINE 5 NEAREST AIRPORTS//////////// 
+  ////////////USE THE COORDINATES(LAT/LON) TO FINE 5 NEAREST AIRPORTS//////////// 
+  ////////////USE THE COORDINATES(LAT/LON) TO FINE 5 NEAREST AIRPORTS//////////// 
+  
+  ////////////TEST////////////
+  if(p->lat == 12.0 && p->lon == 15.0){
+    list.planeListRet_u.airp.p = "You're in Seattle: Here's some test output";
+    list.planeListRet_u.airp.port1 = "You're in Seattle: Here's some test output";
+    list.planeListRet_u.airp.port2 = "You're in Seattle: Here's some test output";
+    list.planeListRet_u.airp.port3 = "You're in Seattle: Here's some test output";
+    list.planeListRet_u.airp.port4 = "You're in Seattle: Here's some test output";
+    list.planeListRet_u.airp.port5 = "You're in Seattle: Here's some test output";
+  }else{
+    list.planeListRet_u.airp.p = "code: BRW, name: Barrow, state: AK, distance: 2698.693567";
+    list.planeListRet_u.airp.port1 = "code: BRW, name: Barrow, state: AK, distance: 2698.693567";
+    list.planeListRet_u.airp.port2 = "code: BRW, name: Barrow, state: AK, distance: 2698.693567";
+    list.planeListRet_u.airp.port3 = "code: BRW, name: Barrow, state: AK, distance: 2698.693567";
+    list.planeListRet_u.airp.port4 = "code: BRW, name: Barrow, state: AK, distance: 2698.693567";
+    list.planeListRet_u.airp.port5 = "code: BRW, name: Barrow, state: AK, distance: 2698.693567";
+  }
+  ////////////TEST////////////
+}
+
+
+
+
+
+ 
+  
--- plane_server.c
+++ plane_server.c
@@ -2,7 +2,12 @@
 #include "places.h"
 #include <errno.h>
 
+#include <cstring>//DELETE WHEN TEST CASE NO LONGER NEEDED
+
+//Copies heap allocated planeListRet into stack allocated version
 void ptrToStatic(planeListRet &s, planeListRet *ptr);
+//Takes cityname and state name and sets lat and lon of that location
+void setCoords(location *loc, double &lat, double &lon);
 
 planeListRet *
 query_places_1_svc(location *argp, struct svc_req *rqstp)
@@ -13,8 +18,8 @@ query_places_1_svc(location *argp, struct svc_req *rqstp)
   planeListRet *resultPtr = nullptr;
   CLIENT *clnt = nullptr;
   position query_airports_1_arg;//just two doubles
-  
-  //Client code///////////  
+  //Use the city and state name to find client coordinates 
+  setCoords(argp, query_airports_1_arg.lat, query_airports_1_arg.lon);
 
   #ifndef	DEBUG	
   clnt = clnt_create (argp->host, AIRPORT_LOOKUP_PROG, AIRPORT_LOOKUP_VERS, "udp");
@@ -29,8 +34,6 @@ query_places_1_svc(location *argp, struct svc_req *rqstp)
   clnt_freeres(clnt, (xdrproc_t)xdr_planeListRet, (char *)&result_1); 
   clnt_destroy(clnt);
   #endif
-  
-  //Sever code///////////
 
   ptrToStatic(result_1, resultPtr);  
   result_1.err = errno;
@@ -45,3 +48,25 @@ void ptrToStatic(planeListRet &s, planeListRet *ptr){
   s.planeListRet_u.airp.port4 = ptr->planeListRet_u.airp.port4;
   s.planeListRet_u.airp.port5 = ptr->planeListRet_u.airp.port5;
 } 
+
+void setCoords(location *loc, double &lat, double &lon){
+  ////////////IMPLEMENT THIS FUNCTION//////////// 
+  ////////////USE THE LOCATION(CITY/STATE) TO FIND COORDINATES(LAT/LON)//////////// 
+  ////////////USE THE LOCATION(CITY/STATE) TO FIND COORDINATES(LAT/LON)//////////// 
+  ////////////USE THE LOCATION(CITY/STATE) TO FIND COORDINATES(LAT/LON)//////////// 
+  
+  ////////////TEST////////////
+  if(std::strcmp(loc->place, "Seattle") == 0){
+     lat = 12;
+  }else{
+     lat = 0;
+     exit(1);
+  }
+  
+  if(std::strcmp(loc->state, "WA") == 0){
+     lon = 15;
+   }else{
+     lon = 0;
+  }
+  ////////////TEST////////////
+}
