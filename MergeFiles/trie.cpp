c8e19c9b717c8daee725566ad5b7db46c9a3a7db
Jack Arnold <jack.arnold021@gmail.com>
2019-11-12 16:56:58 -0800

    Add files via upload
    AUTOCOMPLETE

--- MergeFiles/trie.cpp
+++ MergeFiles/trie.cpp
@@ -1,74 +1,173 @@
 #include <bits/stdc++.h>
 using namespace std;
-
-const int ALPHABET_SIZE = 32;
+//extern int count;                        
+const int ALPHABET_SIZE = 57;
 const int RETURN_ARR_SIZE = 2;
+const char ALPHA[ALPHABET_SIZE]= {'A','B','C','D','E','F','G','H','I','J'
+                            ,'K','L','M','N','O','P','Q','R','S','T'
+                            ,'U','V','W','X','Y','Z','a','b','c','d','e'
+                            ,'f','g','h','i','j','k','l','m','n','o','p'
+                            ,'q','r','s','t','u','v','w','x','y','z'
+                                  ,' ','.','-','(',')'};
+
 struct position{
   float lon;
   float lat;
 };
-
 struct TrieNode
-{
-  struct TrieNode *children[ALPHABET_SIZE];
-  // isEndOfWord is true if the node
-  // represents end of a word
+{  
+  struct TrieNode *children[ALPHABET_SIZE]; //array of potential children
+  //isEndOfWord is true if the node
+  //represents end of a word
   bool isEndOfWord;
-  position * location;
+  position location;
 };
 struct TrieNode *getNode(void)
 {
-  struct TrieNode *pNode =  new TrieNode;
-
+  //  cout << "IN GET NODE" << endl;
+  struct TrieNode *pNode =  new TrieNode;//allocates space for new node entry
   pNode->isEndOfWord = false;
-
-  for (int i = 0; i < ALPHABET_SIZE; i++)
+  
+  for (int i = 0; i < ALPHABET_SIZE; i++) //sets all child nodes to null
     pNode->children[i] = NULL;
-
   return pNode;
+}
+int findIndexInAlpha(char * letter) //helper function to find 
+{
+  //  cout << "IN INDEX HELPER" << endl;
+  // cout << "letter: ";
+  // cout << *letter << endl;  
+  for(int i = 0;i<ALPHABET_SIZE;i++)
+    {
+      //      cout << i << endl;
+      // cout << *&ALPHA[i];
+      if(*letter==*&ALPHA[i])
+        {         
+          //          cout << "XD" << endl; 
+          return i;
+        }
+    }
+  return -1;
 }
 void insert(struct TrieNode *root, string key, float lat, float lon)
 {
+  //  cout << key << endl;
+  // cout << "In insert" << endl;
+  //  count++;
+  //  cout << count << endl;
+  
   struct TrieNode *pCrawl = root; 
   for (int i = 0; i < key.length(); i++)
     {
-      int index = key[i] - 'a';
-      if (!pCrawl->children[index])
-        pCrawl->children[index] = getNode();
-      pCrawl->location->lat = pCrawl->location->lon = -200;
-      pCrawl = pCrawl->children[index];
+      //  cout << "KEY I" ;
+      // cout << key[i] << endl;
+      
+      int index = findIndexInAlpha(&key[i]);
+      // int index = key[i] - 'a';
+      if(index!=-1)
+        {
+          // cout << "MEME" << endl;
+          if (!pCrawl->children[index])
+            pCrawl->children[index] = getNode();
+          pCrawl->location.lat = pCrawl->location.lon = -200;
+          pCrawl = pCrawl->children[index];
+        }
     }
   // mark last node as leaf
   pCrawl->isEndOfWord = true;
-  pCrawl->location->lat = lat;
-  pCrawl->location->lon = lon;
+  pCrawl->location.lat = lat;
+  pCrawl->location.lon = lon;
 }
-float* search(struct TrieNode *root, string key, float arr[])
+struct returnSearch
 {
+  float* arr;
+  string output;
+};
+string formatOutput(struct returnSearch done)
+{
+
+  string finalOutput;
+  int i = 2;
+  while(*&done.output[i]!=' ' || *&done.output[i+1]!=' ')
+    {
+      finalOutput+=done.output[i];
+      i++;
+    }
+  finalOutput+=", ";
+  for(int j=0;j < 2;j++)
+    finalOutput+=done.output[j];
+  finalOutput+=": ";
+  finalOutput+= to_string(done.arr[0]);
+  finalOutput+= ", ";
+  finalOutput+= to_string(done.arr[1]);
+  return finalOutput;
+}
+returnSearch search(struct TrieNode *root, string key, float arr[])
+{
+  returnSearch returnMe;
+  returnMe.arr = NULL;
+  returnMe.output = "";
+  string output = "";
   arr[0]=arr[1]=-1;
   struct TrieNode *pCrawl = root;
-  // struct TrieNode *returnMe = NULL;
   for (int i = 0; i < key.length(); i++)
     {
-      int index = key[i] - 'a';
-      if (!pCrawl->children[index])
-        return arr;      
-      pCrawl = pCrawl->children[index];
+      int index = findIndexInAlpha(&key[i]); // - 'a';
+      if(index!=-1)
+        {
+          if (!pCrawl->children[index])
+            return returnMe;
+          output += ALPHA[index];
+          pCrawl = pCrawl->children[index];
+        }
     }
+  
+  while(pCrawl != NULL && !pCrawl->isEndOfWord)
+    {
+      bool done = 0;
+      for (int i = 0; i < ALPHABET_SIZE;i++)
+        {
+          if(pCrawl->children[i]!=NULL && !done && !pCrawl->isEndOfWord)
+            {
+              done = 1;
+              output+=ALPHA[i];
+              pCrawl=pCrawl->children[i];
+            }
+        }
+    }
+     
   if (pCrawl != NULL && pCrawl->isEndOfWord)
     {
-      arr[0] = pCrawl->location->lat;
-      arr[1] = pCrawl->location->lon;
-      return arr;
+      arr[0] = pCrawl->location.lat;
+      arr[1] = pCrawl->location.lon;
+      returnMe.arr = arr;
+      returnMe.output = output;
+      return returnMe;
     }
-  return arr;
+  return returnMe;
 }
+
+string formatInput(string line)
+{
+  string input;
+  for(int i = 0; i<2; i++)
+    input += line[i];
+  for(int i = 9; i<54;i++)
+    input += line[i];
+  return input;
+}
+float formatlat(string line)
+{
+  return atof(line.substr(143,9).c_str());
+}
+float formatlon(string line)
+{
+  return atof(line.substr(153,11).c_str());
+}
+  
 int main()
 {
-  // Input keys (use only 'a' through 'z'
-  // and lower case)
-  
-  string placename[] = {""}; //stored as STATE CODE + FULL NAME ex. WASeattle City
+  string placename = ""; //stored as STATE CODE + FULL NAME ex. WASeattle City
   string city;
   float lati;
   float longi;
@@ -78,39 +177,17 @@ int main()
   string line;
   while(getline(file,line))
     {
-      placename[0] = "";
-      for(int i = 0; i<3; i++)
-        placename[0] += line[i];
-      for(int i = 143; i<153;i++)
-        placename[0] += line[i];
-      
+      placename = formatInput(line);
       // string placename = line.substr(0,2)+line.substr(9,63);
-      lati = atof(line.substr(143,9).c_str());
-      longi = atof(line.substr(154,10).c_str());
-      insert(root, placename[0], lati, longi);
+      lati = formatlat(line);
+      longi = formatlon(line);
+      insert(root, placename, lati, longi);
     }
-      
   
+  float returnTo[2] = {0,0};
+  struct returnSearch done = search(root, "NJPrinceton", returnTo);
 
-
-
-  // string keys[] = {"the", "a", "there",
-  //               "answer", "any", "by",
-  //               "bye", "their" };
-
-  //  int n = sizeof(keys)/sizeof(keys[0]);
-  
-  // struct TrieNode *root = getNode();
-  
-  // Construct trie
-  // for (int i = 0; i < n; i++)
-  //  insert(root, keys[i]);
-
-  // Search for different keys
-  // search(root, "NJPrinceton bourough")? cout << "Yes\n" :
-  //  cout << "No\n";
-  // search(root, "these")? cout << "Yes\n" :
-  //  cout << "No\n";
+  cout << formatOutput(done);
   return 0;
 }
  
